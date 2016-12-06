adc6d4316e059b73b75a7fafd827ea3caad9df17
davidkim205 <davidkim205@gmail.com>
2016-12-07 05:35:00 +0900

    add led control libraries/LedControl add bluetooth hm-10 (now working) add Dot Matrix Module MAX7219 (now working)

---
+++ module/Bluetooth 4.0 BLE HM-10 working/sketch_HM-10_example/sketch_HM-10_example.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(2, 3); // SoftwareSerial(RX, TX)
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Hello!");
+  // set the data rate for the BT port
+  BTSerial.begin(9600);
+}
+void loop() {
+  while (BTSerial.available()) { // if BT sends something
+    byte data = BTSerial.read();
+    Serial.write(data); // write it to serial(serial monitor)
+  }
+  while (Serial.available()) { // if Serial has input(from serial monitor)
+    byte data = Serial.read();
+    BTSerial.write(data); // write it to BT
+  }
+}
