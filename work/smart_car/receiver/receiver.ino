cf1f40cb1790bf02b6d4da78f5250f05566a185f
davidkim205 <davidkim205@gmail.com>
2017-02-25 06:44:09 +0900

    update

--- work/smart_car/receiver/receiver.ino
+++ work/smart_car/receiver/receiver.ino
@@ -26,8 +26,60 @@ const int AIB = 5;
 
 const int BIA = 6;
 const int BIB = 7;
+//////////////////////////////////
+// MsgFormat
+#define AXIS byte
 
+#define MSG_VER_JOYSTICK 1
+
+struct JoyStickMsgFormat
+{
+  byte version;
+  bool sel : 1; //jostick pressed/released
+  bool left : 1;
+  bool right : 1;
+  bool up: 1;
+  bool down: 1;
+  bool opt1: 1;
+  bool opt2: 1;
+  AXIS x;
+  AXIS y;
+  short raw_x;
+  short raw_y;
+};
+JoyStickMsgFormat g_joystickMsg;
+
+void printJoystick()
+{
+  Serial.print("v:");
+  Serial.print(g_joystickMsg.version);
+  Serial.print(" sel:");
+  Serial.print(g_joystickMsg.sel);
+  Serial.print(" left:");
+  Serial.print(g_joystickMsg.left);
+  Serial.print(" right:");
+  Serial.print(g_joystickMsg.right);
+  Serial.print(" up:");
+  Serial.print(g_joystickMsg.up);
+  Serial.print(" down:");
+  Serial.print(g_joystickMsg.down);
+  Serial.print(" opt1:");
+  Serial.print(g_joystickMsg.opt1);
+  Serial.print(" opt2:");
+  Serial.print(g_joystickMsg.opt2);
+  Serial.print(" x:");
+  Serial.print(g_joystickMsg.x, DEC);
+  Serial.print(" y:");
+  Serial.print(g_joystickMsg.y, DEC);
+  Serial.print(" raw_x:");
+  Serial.print(g_joystickMsg.raw_x);
+  Serial.print(" raw_y:");
+  Serial.print(g_joystickMsg.raw_y);
+
+  Serial.println(" ");
+}
 ///////////////////////////////////////////////
+
 void setup_rf() {
   Serial.println("RECEIVE ROLE");
   radio.begin();
@@ -49,8 +101,8 @@ void loop_rf() {
     //    Serial.print(size);
     //    Serial.print(", data=");
     //    Serial.println(buffer);
-    radio.read( &time, 4 );
-    Serial.println(time);
+    radio.read( &g_joystickMsg, sizeof(g_joystickMsg) );
+    printJoystick();
   }
 }
 ///////////////////////////////////////////////
@@ -58,11 +110,42 @@ void setup_servo() {
   myservo.attach(3);  // attaches the servo on pin 9 to the servo object
 }
 
-void loop_servo(int angle) {
-  Serial.print("loop_servo angle=");
+void loop_servo() {
+  switch (g_joystickMsg.x / 10)
+  {
+    case 0: servo_set(0); break;
+    case 1: servo_set(30); break;
+    case 2: servo_set(30); break;
+    case 3: servo_set(30); break;
+    case 4: servo_set(50); break;
+    case 5: servo_set(50); break;
+    case 6: servo_set(50); break;
+    case 7: servo_set(70); break;
+    case 8: servo_set(70); break;
+    case 9: servo_set(90); break;
+    case 10: servo_set(90); break;
+    case 11: servo_set(100); break;
+    case 12: servo_set(100); break;
+    case 13: servo_set(130); break;
+    case 14: servo_set(130); break;
+    case 15: servo_set(130); break;
+    case 16: servo_set(150); break;
+    case 17: servo_set(160); break;
+    case 18: servo_set(160); break;
+    case 19: servo_set(180); break;
+    case 20: servo_set(180); break;
+  }
+
+}
+int old_angle;
+void servo_set(int angle) {
+  if (old_angle == angle)
+    return;
+  old_angle = angle;
+  Serial.print("servo_set angle=");
   Serial.println(angle);
   myservo.write(angle);
-  delay(15);
+  //delay(15);
 }
 ///////////////////////////////////////////////
 void setup_motor() {
@@ -72,10 +155,10 @@ void setup_motor() {
   pinMode(AIB, OUTPUT);
   pinMode(BIA, OUTPUT);
   pinMode(BIB, OUTPUT);
-  stop();
+  motor_stop();
 }
 
-void forward()
+void motor_forward()
 {
   Serial.println("forward");
   digitalWrite(AIA, HIGH);
@@ -85,7 +168,7 @@ void forward()
   digitalWrite(BIB, LOW);
 
 }
-void backward()
+void motor_backward()
 {
   Serial.println("backward");
   digitalWrite(AIA, LOW);
@@ -95,7 +178,7 @@ void backward()
   digitalWrite(BIB, HIGH);
 
 }
-void stop()
+void motor_stop()
 {
   Serial.println("stop");
   digitalWrite(AIA, LOW);
@@ -105,45 +188,67 @@ void stop()
   digitalWrite(BIB, LOW);
 
 }
+int motor_cmd_old;
+int motor_cmd;
+void loop_motor()
+{
+  if (g_joystickMsg.up) {
+    motor_cmd = 1;
+  } else if (g_joystickMsg.down) {
+    motor_cmd = 2;
+  } else {
+    motor_cmd = 0;
+  }
+  if (motor_cmd_old == motor_cmd)
+    return;
+  motor_cmd_old = motor_cmd;
 
+  if (motor_cmd == 1)
+    motor_forward();
+  else if (motor_cmd == 2)
+    motor_backward();
+  else
+    motor_stop();
+}
 ///////////////////////////////////////////////
 void setup() {
   Serial.begin(9600);
   setup_rf();
   setup_servo();
   setup_motor();
-  
-  loop_servo(90);
-  stop();
+
+  servo_set(90);
+  motor_stop();
 }
 char value;
 int count = 0;
 void loop() {
-  if (Serial.available()) {
-    value = Serial.read();
-    if (value >= '0' && value <= '9')
-    {
-      value -= '0';
-      pos = map((int)value, 0, 9, 0, 180);
-      Serial.print("value=");
-      Serial.print(value, DEC);
-      Serial.print(", pos=");
-      Serial.println(pos);
-      loop_servo(pos);
-    }else
-    {
-      if (value == 'f')
-      {
-        forward();
-      }else if(value == 'b')
-      {
-        backward();
-      }else if(value == 's')
-      {
-        stop();
-      }
-    }
-  }
+  //  if (Serial.available()) {
+  //    value = Serial.read();
+  //    if (value >= '0' && value <= '9')
+  //    {
+  //      value -= '0';
+  //      pos = map((int)value, 0, 9, 0, 180);
+  //      Serial.print("value=");
+  //      Serial.print(value, DEC);
+  //      Serial.print(", pos=");
+  //      Serial.println(pos);
+  //      loop_servo(pos);
+  //    }else
+  //    {
+  //      if (value == 'f')
+  //      {
+  //        forward();
+  //      }else if(value == 'b')
+  //      {
+  //        backward();
+  //      }else if(value == 's')
+  //      {
+  //        stop();
+  //      }
+  //    }
+  //  }
   loop_rf();
-  //loop_servo();
+  loop_servo();
+  loop_motor();
 }
--- work/smart_car/transmitter/transmitter.ino
+++ work/smart_car/transmitter/transmitter.ino
@@ -7,12 +7,78 @@
 #include <SPI.h>
 #include "RF24.h"
 //////////////////////////////////
+// Joystick
+const byte PIN_BUTTON_SELECT = 8;
+const byte PIN_BUTTON_F = 7;
+const byte PIN_BUTTON_E = 6;
+const byte PIN_BUTTON_RIGHT = 3;
+const byte PIN_BUTTON_UP = 2;
+const byte PIN_BUTTON_DOWN = 4;
+const byte PIN_BUTTON_LEFT = 5;
+const byte PIN_ANALOG_X = 0;
+const byte PIN_ANALOG_Y = 1;
+
+//////////////////////////////////
+// MsgFormat
+#define AXIS byte
+
+#define MSG_VER_JOYSTICK 1
+
+struct JoyStickMsgFormat
+{
+  byte version;
+  bool sel : 1; //jostick pressed/released
+  bool left : 1;
+  bool right : 1;
+  bool up: 1;
+  bool down: 1;
+  bool opt1:1;
+  bool opt2:1;
+  AXIS x;
+  AXIS y; 
+  short raw_x;
+  short raw_y;
+};
+JoyStickMsgFormat g_joystickMsg;
+
+void printJoystick(bool ln)
+{
+  Serial.print("v:");
+  Serial.print(g_joystickMsg.version);
+  Serial.print(" sel:");
+  Serial.print(g_joystickMsg.sel);
+  Serial.print(" left:");
+  Serial.print(g_joystickMsg.left);
+  Serial.print(" right:");
+  Serial.print(g_joystickMsg.right);
+  Serial.print(" up:");
+  Serial.print(g_joystickMsg.up);
+  Serial.print(" down:");
+  Serial.print(g_joystickMsg.down);
+  Serial.print(" opt1:");
+  Serial.print(g_joystickMsg.opt1);
+  Serial.print(" opt2:");
+  Serial.print(g_joystickMsg.opt2);
+  Serial.print(" x:");
+  Serial.print(g_joystickMsg.x, DEC);
+  Serial.print(" y:");
+  Serial.print(g_joystickMsg.y, DEC); 
+  Serial.print(" raw_x:");
+  Serial.print(g_joystickMsg.raw_x);
+  Serial.print(" raw_y:");
+  Serial.print(g_joystickMsg.raw_y);
+  if (ln)
+    Serial.println(" ");
+}
+//////////////////////////////////
 // RF
 // Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8
 RF24 radio(9, 10);
 byte addresses[][6] = {"1Node", "2Node"};
 unsigned long time;
 String str_time;
+
+
 ///////////////////////////////////////
 
 void setup_rf() {
@@ -25,6 +91,7 @@ void setup_rf() {
   // Start the radio listening for data
   radio.startListening();
 }
+
 void loop_rf() {
   radio.stopListening();
   time = millis();
@@ -32,17 +99,71 @@ void loop_rf() {
   // Variable for the received timestamp
   Serial.print("send=");
   Serial.print(", size=");
-  Serial.print(sizeof(long));
-  Serial.print(", data=");
-  Serial.println(time);
-  radio.write( &time, sizeof(long) );
+  Serial.print(sizeof(g_joystickMsg));
+  Serial.println(" ");
+  radio.write( &g_joystickMsg, sizeof(g_joystickMsg) );
 }
 ///////////////////////////////////////////////
+void setup_joystick() {
+  pinMode(PIN_BUTTON_RIGHT, INPUT);
+  digitalWrite(PIN_BUTTON_RIGHT, HIGH);
+  pinMode(PIN_BUTTON_LEFT, INPUT);
+  digitalWrite(PIN_BUTTON_LEFT, HIGH);
+  pinMode(PIN_BUTTON_UP, INPUT);
+  digitalWrite(PIN_BUTTON_UP, HIGH);
+  pinMode(PIN_BUTTON_DOWN, INPUT);
+  digitalWrite(PIN_BUTTON_DOWN, HIGH);
+  pinMode(PIN_BUTTON_SELECT, INPUT);
+  digitalWrite(PIN_BUTTON_SELECT, HIGH);
+  pinMode(PIN_BUTTON_E, INPUT);
+  digitalWrite(PIN_BUTTON_E, HIGH);
+  pinMode(PIN_BUTTON_F, INPUT);
+  digitalWrite(PIN_BUTTON_F, HIGH);
+}
+short raw_x;
+short raw_y;
+bool sel;
+bool left;
+bool right;
+bool up;
+bool down;
+bool opt1;
+bool opt2;
+
+void loop_joystick() {
+  up = digitalRead(PIN_BUTTON_UP);
+  right = digitalRead(PIN_BUTTON_RIGHT);
+  down = digitalRead(PIN_BUTTON_DOWN);
+  left = digitalRead(PIN_BUTTON_LEFT);
+  sel = digitalRead(PIN_BUTTON_SELECT);
+  opt1 = digitalRead(PIN_BUTTON_F);
+  opt2 = digitalRead(PIN_BUTTON_E);
+  raw_x = analogRead(PIN_ANALOG_X);
+  raw_y = analogRead(PIN_ANALOG_Y);
+
+  g_joystickMsg.version = MSG_VER_JOYSTICK;
+  g_joystickMsg.up = up?0:1;
+  g_joystickMsg.right = right?0:1;
+  g_joystickMsg.down = down?0:1;
+  g_joystickMsg.left = left?0:1;
+  g_joystickMsg.sel = sel?0:1;
+  g_joystickMsg.opt1 = opt1?0:1;
+  g_joystickMsg.opt2 = opt2?0:1;
+  g_joystickMsg.raw_x = raw_x;
+  g_joystickMsg.raw_y = raw_y;
+  g_joystickMsg.x = map(raw_x, 0, 700, 0, 200);
+  g_joystickMsg.y = map(raw_y, 0, 700, 0, 200);
+  printJoystick(false);
+}
+///////////////////////////////////////////////
+
 void setup() {
   Serial.begin(9600);
   setup_rf();
+  setup_joystick();
 }
 void loop() {
+  loop_joystick();
   loop_rf();
 }
 
